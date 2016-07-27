ef202a53a8f53686de1e06b7fcc9fca70b0c8417
jason <jason@idarex.com>
2016-07-27 14:56:50 +0800

    ss

--- index.php
+++ index.php
@@ -4,9 +4,12 @@ phpinfo()
 $str = '哈哈哈哈哈哈哈哈';
 echo $str;
 echo '这是jason分支的内容';
+<<<<<<< 70abcf00cf9614cfcd4b9bfad6f26c5abef84ad6
 echo 'this is jason1';
 
 echo "测试冲出1,jason分支";
+=======
+>>>>>>> jason
 
 
 
